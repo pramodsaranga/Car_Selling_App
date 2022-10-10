e4175b0ba938613bbeb18ece88d75bb152bda810
pramodsaranga <pramodsaranga31@gmail.com>
2022-10-10 22:47:06 +0530

    added data page and updated content

--- screens/Data.js
+++ screens/Data.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Login() {
+export default function Data() {
   return (
     <View>
-      <Text>Login</Text>
+      <Text>Data</Text>
     </View>
   )
 }
