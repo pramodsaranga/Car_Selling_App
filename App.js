3f77baa4413cccd19974b46ae8472ebeb87d4ce1
pramodsaranga <pramodsaranga31@gmail.com>
2022-10-10 22:59:00 +0530

    Updated content

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Register from './screens/Register'
+import Login from './screens/Login'
+import Home from './screens/Home'
+import CarDetails from './screens/CarDetails'
+import AddCar from './screens/AddCar'
+import AddData from './screens/AddData'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="CarDetails" component={CarDetails} />
+      <Stack.Screen name="AddCar" component={AddCar} />
+      <Stack.Screen name="AddData" component={AddData} />
+      
+    </Stack.Navigator>
+  </NavigationContainer>
+    
   )
 }
---
+++ screens/CarDetails.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CarDetails() {
+  return (
+    <View>
+      <Text>CarDetails</Text>
+    </View>
+  )
+}
